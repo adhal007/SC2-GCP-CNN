3ff6fa08d8deb770d099bce06d50be1d4135a3e9
Abhilash Dhal <adhal@ucdavis.edu>
2019-03-20 15:16:17 -0700

    Colab implementation

---
+++ final_CNN_model/Colab-Files/__init__.py
@@ -0,0 +1,15 @@
+!pip install -U -q PyDrive
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+# 1. Authenticate and create the PyDrive client.
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+from google.colab import drive
+drive.mount('/content/drive')
